9e55bbac92e2d85324e372bec0cbab35b6164401
Dario Castiglione <dario.castiglione99@gmail.com>
2022-02-12 15:25:41 +0100

    asd

--- components/Layouts/index.jsx
+++ components/Layouts/index.jsx
@@ -1,11 +1,11 @@
 
 import Footer from "../Footer";
-import NavBar from "../Navbar";
+
 import SideMenu from "../SideMenu";
 export default function Layout({ children }) {
     return (
       <>
-        <NavBar />
+        
         <SideMenu />
         {children}
         <Footer />
