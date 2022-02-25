793087ba14c5645d9f453c563b437714eab1e6ae
Dario Castiglione <dario.castiglione99@gmail.com>
2022-02-25 10:43:30 +0100

    fix

--- translations/i18n.js
+++ translations/i18n.js
@@ -1,12 +1,12 @@
 
 import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next'; 
+import { initReactI18next } from 'react-i18next';
+ 
 import IT_TRANSLATION from './it/translation';
 import EN_TRANSLATION from './en/translation';
 
 
 i18n
-
     .use(initReactI18next)
     .init(
     {
