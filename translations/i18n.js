90e51575de022a244003f12ffa22501e8208d5a2
Dario Castiglione <dario.castiglione99@gmail.com>
2022-02-25 10:31:16 +0100

    fixx

--- translations/i18n.js
+++ translations/i18n.js
@@ -1,14 +1,12 @@
 
 import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languageDetector';
- 
+import { initReactI18next } from 'react-i18next'; 
 import IT_TRANSLATION from './it/translation';
 import EN_TRANSLATION from './en/translation';
 
 
 i18n
-    .use(LanguageDetector)
+
     .use(initReactI18next)
     .init(
     {
